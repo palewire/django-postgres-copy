7bda31996d1b1ae8f12fd662e664498b23406861
palewire <ben.welsh@gmail.com>
2015-07-15 13:03:26 -0700

    Python classifiers in setup.py

--- setup.py
+++ setup.py
@@ -46,4 +46,10 @@ setup(
     license="MIT",
     packages=("postgres_copy",),
     cmdclass={'test': TestCommand}
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
 )
