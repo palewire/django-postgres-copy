2aa0e69458748ae0a96dc896d10116cce3a7f6d6
palewire <ben.welsh@gmail.com>
2015-07-15 14:21:01 -0700

    Corrected syntax error in setup.py

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ class TestCommand(Command):
 setup(
     name='django-postgres-copy',
     version='0.0.3',
-    description='Quickly load comma-delimited data into a Django model using PostgreSQL's COPY command',
+    description='Quickly load comma-delimited data into a Django model using PostgreSQL\'s COPY command',
     author='Ben Welsh',
     author_email='ben.welsh@gmail.com',
     url='http://www.github.com/california-civic-data-coalition/django-postgresql-copy/',
