6e68a06e79eb90df93eb89f298cce28653e5435e
palewire <ben.welsh@gmail.com>
2017-02-22 09:24:02 -0800

    v0.1.1

--- postgres_copy/__init__.py
+++ postgres_copy/__init__.py
@@ -6,7 +6,7 @@ import csv
 from collections import OrderedDict
 from django.db import connections, router
 from django.contrib.humanize.templatetags.humanize import intcomma
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 
 class CopyMapping(object):
