e9d871929d67f0c9405120a6681346a8a7ac117e
palewire <ben.welsh@gmail.com>
2017-04-24 15:35:40 -0700

    v0.2.1

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ class TestCommand(Command):
 
 setup(
     name='django-postgres-copy',
-    version='0.2.0',
+    version='0.2.1',
     description="Quickly load comma-delimited data into a Django model using PostgreSQL's COPY command",
     author='Ben Welsh',
     author_email='ben.welsh@gmail.com',
