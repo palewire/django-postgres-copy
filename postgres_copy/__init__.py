00690756c55e3ee8b9ed6cd5acdf9e38f817d9a4
palewire <ben.welsh@gmail.com>
2017-12-31 15:39:04 -0800

    Moved managers into a separate file for expansion

--- postgres_copy/__init__.py
+++ postgres_copy/__init__.py
@@ -1,54 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from django.db import models
-from django.db import connection
 from .copy_from import CopyMapping
 from .copy_to import SQLCopyToCompiler, CopyToQuery
+from .managers import CopyManager, CopyQuerySet
 __version__ = '2.0.0'
-
-
-class CopyQuerySet(models.QuerySet):
-    """
-    Subclass of QuerySet that adds from_csv and to_csv methods.
-    """
-    def from_csv(self, csv_path, mapping=None, **kwargs):
-        """
-        Copy CSV file from the provided path to the current model using the provided mapping.
-        """
-        mapping = CopyMapping(self.model, csv_path, mapping, **kwargs)
-        mapping.save(silent=True)
-
-    def to_csv(self, csv_path, *fields, **kwargs):
-        """
-        Copy current QuerySet to CSV at provided path.
-        """
-        try:
-            # For Django 2.0 forward
-            query = self.query.chain(CopyToQuery)
-        except AttributeError:
-            # For Django 1.11 backward
-            query = self.query.clone(CopyToQuery)
-
-        # Get fields
-        query.copy_to_fields = fields
-
-        # Delimiter
-        query.copy_to_delimiter = kwargs.get('delimiter', ',')
-
-        # Header?
-        with_header = kwargs.get('header', True)
-        query.copy_to_header = "HEADER" if with_header else ""
-
-        # Null string
-        null_string = kwargs.get('null', None)
-        query.copy_to_null_string = "" if null_string is None else "NULL '%s'" % null_string
-
-        # Run the query
-        compiler = query.get_compiler(self.db, connection=connection)
-        compiler.execute_sql(csv_path)
-
-
-CopyManager = models.Manager.from_queryset(CopyQuerySet)
 
 
 __all__ = (
@@ -56,5 +11,5 @@ __all__ = (
     'CopyMapping',
     'CopyQuerySet',
     'CopyToQuery',
-    'SQLCopyToCompiler',
+    'SQLCopyToCompiler'
 )
---
+++ postgres_copy/managers.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from django.db import models
+from django.db import connection
+from .copy_to import CopyToQuery
+from .copy_from import CopyMapping
+
+
+class CopyQuerySet(models.QuerySet):
+    """
+    Subclass of QuerySet that adds from_csv and to_csv methods.
+    """
+    def from_csv(self, csv_path, mapping=None, **kwargs):
+        """
+        Copy CSV file from the provided path to the current model using the provided mapping.
+        """
+        mapping = CopyMapping(self.model, csv_path, mapping, **kwargs)
+        mapping.save(silent=True)
+
+    def to_csv(self, csv_path, *fields, **kwargs):
+        """
+        Copy current QuerySet to CSV at provided path.
+        """
+        try:
+            # For Django 2.0 forward
+            query = self.query.chain(CopyToQuery)
+        except AttributeError:
+            # For Django 1.11 backward
+            query = self.query.clone(CopyToQuery)
+
+        # Get fields
+        query.copy_to_fields = fields
+
+        # Delimiter
+        query.copy_to_delimiter = kwargs.get('delimiter', ',')
+
+        # Header?
+        with_header = kwargs.get('header', True)
+        query.copy_to_header = "HEADER" if with_header else ""
+
+        # Null string
+        null_string = kwargs.get('null', None)
+        query.copy_to_null_string = "" if null_string is None else "NULL '%s'" % null_string
+
+        # Run the query
+        compiler = query.get_compiler(self.db, connection=connection)
+        compiler.execute_sql(csv_path)
+
+
+CopyManager = models.Manager.from_queryset(CopyQuerySet)
