20f7c1c393b8d35b727e302797cc2c039595a65d
palewire <ben.welsh@gmail.com>
2017-12-31 16:06:21 -0800

    Merged in my constraints qs but don't have it working yet

--- postgres_copy/managers.py
+++ postgres_copy/managers.py
@@ -1,21 +1,149 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import logging
 from django.db import models
 from django.db import connection
 from .copy_to import CopyToQuery
 from .copy_from import CopyMapping
+logger = logging.getLogger(__name__)
 
 
-class CopyQuerySet(models.QuerySet):
+class ConstraintQuerySet(models.QuerySet):
+    """
+    Utilities for temporarily dropping and restoring constraints and indexes.
+    """
+    @property
+    def constrained_fields(self):
+        """
+        Returns list of model's fields with db_constraint set to True.
+        """
+        return [
+            f for f in self.model._meta.fields
+            if hasattr(f, 'db_constraint') and f.db_constraint
+        ]
+
+    @property
+    def indexed_fields(self):
+        """
+        Returns list of model's fields with db_index set to True.
+        """
+        return [f for f in self.model._meta.fields if f.db_index]
+
+    def edit_schema(self, schema_editor, method_name, args):
+        """
+        Edits the schema without throwing errors.
+
+        This allows for the add and drop methods to be run frequently and without fear.
+        """
+        try:
+            getattr(schema_editor, method_name)(*args)
+        except Exception:
+            logger.debug("Edit unnecessary. Skipped")
+            pass
+
+    def drop_constraints(self):
+        """
+        Drop constraints on the model and its fields.
+        """
+        logger.debug("Dropping constraints from {}".format(self.model.__name__))
+        with connection.schema_editor() as schema_editor:
+            # Remove any "unique_together" constraints
+            if self.model._meta.unique_together:
+                logger.debug("Dropping unique_together of {}".format(self.model._meta.unique_together))
+                args = (self.model, self.model._meta.unique_together, ())
+                self.edit_schema(schema_editor, 'alter_unique_together', args)
+
+            # Remove any field constraints
+            for field in self.constrained_fields:
+                logger.debug("Dropping constraints from {}".format(field))
+                field_copy = field.__copy__()
+                field_copy.db_constraint = False
+                args = (self.model, field, field_copy)
+                self.edit_schema(schema_editor, 'alter_field', args)
+
+    def drop_indexes(self):
+        """
+        Drop indexes on the model and its fields.
+        """
+        logger.debug("Dropping indexes from {}".format(self.model.__name__))
+        with connection.schema_editor() as schema_editor:
+            # Remove any "index_together" constraints
+            logger.debug("Dropping index_together of {}".format(self.model._meta.index_together))
+            if self.model._meta.index_together:
+                args = (self.model, self.model._meta.index_together, ())
+                self.edit_schema(schema_editor, 'alter_index_together', args)
+
+            # Remove any field indexes
+            for field in self.indexed_fields:
+                logger.debug("Dropping indexes from {}".format(field))
+                field_copy = field.__copy__()
+                field_copy.db_index = False
+                args = (self.model, field, field_copy)
+                self.edit_schema(schema_editor, 'alter_field', args)
+
+    def restore_constraints(self):
+        """
+        Restore constraints on the model and its fields.
+        """
+        logger.debug("Adding constraints to {}".format(self.model.__name__))
+        with connection.schema_editor() as schema_editor:
+            # Add any "unique_together" contraints from the database
+            if self.model._meta.unique_together:
+                logger.debug("Adding unique_together of {}".format(self.model._meta.unique_together))
+                args = (self.model, (), self.model._meta.unique_together)
+                self.edit_schema(schema_editor, 'alter_unique_together', args)
+
+            # Add any constraints to the fields
+            for field in self.constrained_fields:
+                logger.debug("Adding constraints to {}".format(field))
+                field_copy = field.__copy__()
+                field_copy.db_constraint = False
+                args = (self.model, field_copy, field)
+                self.edit_schema(schema_editor, 'alter_field', args)
+
+    def restore_indexes(self):
+        """
+        Restore indexes on the model and its fields.
+        """
+        logger.debug("Adding indexes to {}".format(self.model.__name__))
+        with connection.schema_editor() as schema_editor:
+            # Add any "index_together" contraints to the database.
+            if self.model._meta.index_together:
+                logger.debug("Adding index_together of {}".format(self.model._meta.index_together))
+                args = (self.model, (), self.model._meta.index_together)
+                self.edit_schema(schema_editor, 'alter_index_together', args)
+
+            # Add any indexes to the fields
+            for field in self.indexed_fields:
+                logger.debug("Adding indexes to {}".format(field))
+                field_copy = field.__copy__()
+                field_copy.db_index = False
+                args = (self.model, field_copy, field)
+                self.edit_schema(schema_editor, 'alter_field', args)
+
+
+class CopyQuerySet(ConstraintQuerySet):
     """
     Subclass of QuerySet that adds from_csv and to_csv methods.
     """
-    def from_csv(self, csv_path, mapping=None, **kwargs):
+    def from_csv(self, csv_path, mapping=None, drop_constraints=False, drop_indexes=False, **kwargs):
         """
         Copy CSV file from the provided path to the current model using the provided mapping.
         """
         mapping = CopyMapping(self.model, csv_path, mapping, **kwargs)
+
+        if drop_constraints:
+            self.drop_constraints()
+        if drop_indexes:
+            self.drop_indexes()
+
         mapping.save(silent=True)
+
+        if drop_constraints:
+            self.restore_constraints()
+        if drop_indexes:
+            self.restore_indexes()
 
     def to_csv(self, csv_path, *fields, **kwargs):
         """
