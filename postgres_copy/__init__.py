debd86f6574868a515d98ad56526fea7fdf4ed00
palewire <ben.welsh@gmail.com>
2018-02-06 11:46:01 -0800

    v2.3.0

--- postgres_copy/__init__.py
+++ postgres_copy/__init__.py
@@ -3,7 +3,7 @@
 from .copy_from import CopyMapping
 from .copy_to import SQLCopyToCompiler, CopyToQuery
 from .managers import CopyManager, CopyQuerySet
-__version__ = '2.2.0'
+__version__ = '2.3.0'
 
 
 __all__ = (
