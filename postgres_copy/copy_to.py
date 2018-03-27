a500e4b244e0493b59d5f3c28ffb8f06427b17c4
igncampa <ignacio.campabadal@protonmail.com>
2018-03-27 12:40:00 -0600

    fixed inline comments to flake8 specifications

--- postgres_copy/copy_to.py
+++ postgres_copy/copy_to.py
@@ -52,14 +52,14 @@ class SQLCopyToCompiler(SQLCompiler):
             copy_to_sql = \
                 "COPY ({0}) TO STDOUT DELIMITER '{1}' CSV {2} {3} {4} {5} {6} {7}"
             copy_to_sql = copy_to_sql.format(
-                select_sql,                     #0
-                self.query.copy_to_delimiter,   #1
-                self.query.copy_to_header,      #2
-                self.query.copy_to_null_string, #3
-                self.query.copy_to_quote_char,  #4
-                self.query.copy_to_force_quote, #5
-                self.query.copy_to_encoding,    #6
-                self.query.copy_to_escape,      #7
+                select_sql,                      # 0
+                self.query.copy_to_delimiter,    # 1
+                self.query.copy_to_header,       # 2
+                self.query.copy_to_null_string,  # 3
+                self.query.copy_to_quote_char,   # 4
+                self.query.copy_to_force_quote,  # 5
+                self.query.copy_to_encoding,     # 6
+                self.query.copy_to_escape,       # 7
             )
             # then execute
             logger.debug(copy_to_sql)
