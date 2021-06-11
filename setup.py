52a4f7d3c61588fdbd69481658bc34b7ed2dbfe8
palewire <b@palewi.re>
2021-06-11 02:35:45 -0700

    HOST

--- setup.py
+++ setup.py
@@ -24,11 +24,13 @@ class TestCommand(Command):
         settings.configure(
             DATABASES={
                 'default': {
+                    'HOST': 'localhost',
                     'NAME': 'test',
                     'USER': 'postgres',
                     'ENGINE': 'django.db.backends.postgresql_psycopg2'
                 },
                 'other': {
+                    'HOST': 'localhost',
                     'NAME': 'test_alternative',
                     'USER': 'postgres',
                     'ENGINE': 'django.db.backends.postgresql_psycopg2'
@@ -38,6 +40,7 @@ class TestCommand(Command):
                     'ENGINE': 'django.db.backends.sqlite3'
                 },
                 'secondary': {
+                    'HOST': 'localhost',
                     'NAME': 'test_secondary',
                     'USER': 'postgres',
                     'ENGINE': 'django.db.backends.postgresql_psycopg2'
@@ -92,7 +95,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.7',        
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Framework :: Django',
