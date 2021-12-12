01eb895bf84286a26f592055aa074ca78c8effb7
Ben Welsh <b@palewi.re>
2021-12-12 12:11:18 -0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -113,7 +113,9 @@ html_theme = 'alabaster'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {}
+html_theme_options = {
+  'show_powered_by': False,
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
